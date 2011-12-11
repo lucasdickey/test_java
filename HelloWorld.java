1dc726b53cef63173329ba73b4a3c4d8de4373dd
Lucas Dickey <lucas@lucasdickey.com>
2011-12-11 13:10:29 -0800

    added first hello world files

---
+++ HelloWorld.java
@@ -0,0 +1,6 @@
+// output hello world for first java file to test compile and execute
+public class HelloWorld {
+	public static void main (String[] args) {
+		  System.out.println("Hello-fucking-world!");
+	}
+}
